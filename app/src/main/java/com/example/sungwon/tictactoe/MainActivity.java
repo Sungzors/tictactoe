ce692aed3ef82f1e8857bab20285f46d62b266cf
Sungzors <s1chun3636@gmail.com>
2016-08-14 21:37:39 -0700

    Main Activity done

--- app/src/main/java/com/example/sungwon/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/sungwon/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sungwon.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         mPlayer1 = (EditText)findViewById(R.id.player1Name);
         mPlayer2 = (EditText)findViewById(R.id.player2Name);
         mStart = (Button)findViewById(R.id.startButton);
+
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Tictactoe_Layout.class);
+                String s = mPlayer1.getText().toString();
+                String t = mPlayer2.getText().toString();
+                intent.putExtra("player1", s);
+                intent.putExtra("player2", t);
+                startActivity(intent);
+            }
+        });
     }
 }
