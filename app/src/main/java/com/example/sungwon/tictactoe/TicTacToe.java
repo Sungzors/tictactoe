289e03cf6a268b36558a9e010ff6ad8421356d39
Sungzors <s1chun3636@gmail.com>
2016-08-15 11:21:50 -0700

    Tictactoe and other basic functions all complete

---
+++ app/src/main/java/com/example/sungwon/tictactoe/TicTacToe.java
@@ -0,0 +1,167 @@
+package com.example.sungwon.tictactoe;
+
+/**
+ * Created by SungWon on 8/14/2016.
+ */
+public class TicTacToe {
+
+
+    CharSequence mButton1;
+    CharSequence mButton2;
+    CharSequence mButton3;
+    CharSequence mButton4;
+    CharSequence mButton5;
+    CharSequence mButton6;
+    CharSequence mButton7;
+    CharSequence mButton8;
+    CharSequence mButton9;
+
+    public TicTacToe(CharSequence mButton1, CharSequence mButton2, CharSequence mButton3, CharSequence mButton4, CharSequence mButton5, CharSequence mButton6, CharSequence mButton7, CharSequence mButton8, CharSequence mButton9) {
+        this.mButton1 = mButton1;
+        this.mButton2 = mButton2;
+        this.mButton3 = mButton3;
+        this.mButton4 = mButton4;
+        this.mButton5 = mButton5;
+        this.mButton6 = mButton6;
+        this.mButton7 = mButton7;
+        this.mButton8 = mButton8;
+        this.mButton9 = mButton9;
+    }
+
+// String mButton1, String mButton2, String mButton3, String mButton4, String mButton5, String mButton6, String mButton7, String mButton8, String mButton9
+
+
+    public CharSequence getmButton1() {
+        return mButton1;
+    }
+
+    public void setmButton1(CharSequence mButton1){
+        this.mButton1 = mButton1;
+    }
+
+    public CharSequence getmButton2() {
+        return mButton2;
+    }
+
+    public void setmButton2(CharSequence mButton2) {
+        this.mButton2 = mButton2;
+    }
+
+    public CharSequence getmButton3() {
+        return mButton3;
+    }
+
+    public void setmButton3(CharSequence mButton3) {
+        this.mButton3 = mButton3;
+    }
+
+    public CharSequence getmButton4() {
+        return mButton4;
+    }
+
+    public void setmButton4(CharSequence mButton4) {
+        this.mButton4 = mButton4;
+    }
+
+    public CharSequence getmButton5() {
+        return mButton5;
+    }
+
+    public void setmButton5(CharSequence mButton5) {
+        this.mButton5 = mButton5;
+    }
+
+    public CharSequence getmButton6() {
+        return mButton6;
+    }
+
+    public void setmButton6(CharSequence mButton6) {
+        this.mButton6 = mButton6;
+    }
+
+    public CharSequence getmButton7() {
+        return mButton7;
+    }
+
+    public void setmButton7(CharSequence mButton7) {
+        this.mButton7 = mButton7;
+    }
+
+    public CharSequence getmButton8() {
+        return mButton8;
+    }
+
+    public void setmButton8(CharSequence mButton8) {
+        this.mButton8 = mButton8;
+    }
+
+    public CharSequence getmButton9() {
+        return mButton9;
+    }
+
+    public void setmButton9(CharSequence mButton9) {
+        this.mButton9 = mButton9;
+    }
+
+    public boolean hasWon(){
+        boolean winning = false;
+        if (mButton1.toString().equals("O")) {
+            if (mButton4.toString().equals("O") && mButton7.toString().equals("O")) {
+                winning = true;
+            } else if (mButton2.toString().equals("O") && mButton3.toString().equals("O")) {
+                winning = true;
+            } else if (mButton5.toString().equals("O") && mButton9.toString().equals("O")) {
+                winning = true;
+            }
+        } else if (mButton1.toString().equals("X")) {
+            if (mButton4.toString().equals("X") && mButton7.toString().equals("X")) {
+                winning = true;
+            } else if (mButton2.toString().equals("X") && mButton3.toString().equals("X")) {
+                winning = true;
+            } else if (mButton5.toString().equals("X") && mButton9.toString().equals("X")) {
+                winning = true;
+            }
+        }
+        if (mButton2.toString().equals("O")) {
+            if (mButton5.toString().equals("O") && mButton8.toString().equals("O")) {
+                winning = true;
+            }
+        } else if (mButton2.toString().equals("X")) {
+            if (mButton5.toString().equals("X") && mButton8.toString().equals("X")) {
+                winning = true;
+            }
+        }
+        if (mButton3.toString().equals("O")) {
+            if (mButton6.toString().equals("O") && mButton9.toString().equals("O")) {
+                winning = true;
+            } else if (mButton5.toString().equals("O") && mButton7.toString().equals("O")) {
+                winning = true;
+            }
+        } else if (mButton3.toString().equals("X")) {
+            if (mButton6.toString().equals("X") && mButton9.toString().equals("X")) {
+                winning = true;
+            } else if (mButton5.toString().equals("X") && mButton7.toString().equals("X")) {
+                winning = true;
+            }
+        }
+        if (mButton4.toString().equals("O")) {
+            if (mButton5.toString().equals("O") && mButton6.toString().equals("O")) {
+                winning = true;
+            }
+        } else if (mButton4.toString().equals("X")) {
+            if (mButton5.toString().equals("X") && mButton6.toString().equals("X")) {
+                winning = true;
+            }
+        }
+        if (mButton7.toString().equals("O")) {
+            if (mButton8.toString().equals("O") && mButton9.toString().equals("O")) {
+                winning = true;
+            }
+        } else if (mButton7.toString().equals("X")) {
+            if (mButton8.toString().equals("X") && mButton9.toString().equals("X")) {
+                winning = true;
+            }
+        }
+        return winning;
+    }
+}
--- app/src/main/java/com/example/sungwon/tictactoe/Tictactoe_Layout.java
+++ app/src/main/java/com/example/sungwon/tictactoe/Tictactoe_Layout.java
@@ -1,13 +1,265 @@
 package com.example.sungwon.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Tictactoe_Layout extends AppCompatActivity {
+    TextView mPlayerNameT;
+    TextView mTurnWin;
+    Button mButton1;
+    Button mButton2;
+    Button mButton3;
+    Button mButton4;
+    Button mButton5;
+    Button mButton6;
+    Button mButton7;
+    Button mButton8;
+    Button mButton9;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tictactoe__layout);
+        mPlayerNameT = (TextView)findViewById(R.id.playerNameT);
+        mTurnWin = (TextView)findViewById(R.id.turnwin);
+        mButton1 = (Button)findViewById(R.id.button1);
+        mButton2 = (Button)findViewById(R.id.button2);
+        mButton3 = (Button)findViewById(R.id.button3);
+        mButton4 = (Button)findViewById(R.id.button4);
+        mButton5 = (Button)findViewById(R.id.button5);
+        mButton6 = (Button)findViewById(R.id.button6);
+        mButton7 = (Button)findViewById(R.id.button7);
+        mButton8 = (Button)findViewById(R.id.button8);
+        mButton9 = (Button)findViewById(R.id.button9);
+        Intent intent = getIntent();
+        final String p1 = intent.getStringExtra("player1");
+        final String p2 = intent.getStringExtra("player2");
+        CharSequence buttonchar1 = mButton1.getText();
+        CharSequence buttonchar2 = mButton2.getText();
+        CharSequence buttonchar3 = mButton3.getText();
+        CharSequence buttonchar4 = mButton4.getText();
+        CharSequence buttonchar5 = mButton5.getText();
+        CharSequence buttonchar6 = mButton6.getText();
+        CharSequence buttonchar7 = mButton7.getText();
+        CharSequence buttonchar8 = mButton8.getText();
+        CharSequence buttonchar9 = mButton9.getText();
+        final TicTacToe ttt = new TicTacToe(buttonchar1, buttonchar2, buttonchar3, buttonchar4, buttonchar5, buttonchar6, buttonchar7, buttonchar8, buttonchar9);
+
+
+        mPlayerNameT.setText(p1);
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton1("O");
+                    mButton1.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)) {
+                    ttt.setmButton1("X");
+                    mButton1.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton2("O");
+                    mButton2.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton2("X");
+                    mButton2.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton3("O");
+                    mButton3.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton3("X");
+                    mButton3.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton4("O");
+                    mButton4.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton4("X");
+                    mButton4.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton5("O");
+                    mButton5.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton5("X");
+                    mButton5.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton6("O");
+                    mButton6.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton6("X");
+                    mButton6.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton7("O");
+                    mButton7.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton7("X");
+                    mButton7.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton8("O");
+                    mButton8.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton8("X");
+                    mButton8.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
+        mButton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPlayerNameT.getText().toString().equals(p1)) {
+                    ttt.setmButton9("O");
+                    mButton9.setText("O");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p2);
+                    }
+
+                } else if (mPlayerNameT.getText().toString().equals(p2)){
+                    ttt.setmButton9("X");
+                    mButton9.setText("X");
+                    if (ttt.hasWon()) {
+                        mTurnWin.setText(R.string.win);
+                    } else if(!ttt.hasWon()){
+                        mPlayerNameT.setText(p1);
+                    }
+                }
+            }
+        });
     }
 }
